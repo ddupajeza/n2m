06545330db89ecca21224257715a8138b71587ed
Glitch (shorthaired-dietician) <none>
2020-01-20 02:46:36 +0000

    🐬🏐 Checkpoint ./server.js:10502925/10

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
 console.log("Streamstatus by DayZoonHD")
 
-client.user.setActivity(`lia?❤️`, {
+client.user.setActivity(`lia❤️`, {
 type: "STREAMING",
-url: "https://www.twitch.tv/ninja"})
+url: "https://www.twitch.tv/tufe"})
     .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'none'}`))
     .catch(console.error);
 });
