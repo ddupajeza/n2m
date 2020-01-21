7c5cb8e4220b581b399e7767c6c5daab0b974f94
Glitch (shorthaired-dietician) <none>
2020-01-21 04:54:35 +0000

    🛎🏣 Checkpoint ./server.js:10502925/11

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
 console.log("Streamstatus by DayZoonHD")
 
-client.user.setActivity(`lia❤️`, {
+client.user.setActivity(`big 4x`, {
 type: "STREAMING",
 url: "https://www.twitch.tv/tufe"})
     .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'none'}`))
