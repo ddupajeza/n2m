63dfd16b8ceefe6d178a342d261548710ecd422a
Glitch (shorthaired-dietician) <none>
2020-02-13 22:52:12 +0000

    Rewound to commit 611043784f7b5d1bbd0f5ba126bf03b63dd1ff3a + 0 edits

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
+tconst Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
 console.log("Streamstatus by DayZoonHD")
 
-client.user.setActivity(`red light'`, {
+client.user.setActivity(`red light`, {
 type: "STREAMING",
-url: "https://www.twitch.tv/ninja"})
+url: "https://www.twitch.tv/tufe"})
     .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'none'}`))
     .catch(console.error);
 });
 
-client.login('NjQzMTY2NDM3MDUzMDM4NjIy.XiIBJg.new1yJiDqYjtq6UeiUGUPWMXDT4');
+client.login('NjQzMTY2NDM3MDUzMDM4NjIy.XkHb9g.UvA4DvFt9GAZWIVr2sslw4ZbJt8');
